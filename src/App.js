89d81809d249edea3280081bd884be69236a7e43
vijay <sarojvijay1971998@gmail.com>
2023-07-29 15:17:00 +0530

    add home page and login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {name,email} from './index'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>HomePage</h1>
+        <form>
+        <h3>Login Page</h3>
+        <input type="text" value={name} placeholder='YourName'></input>
+        <br/>
+        <input type="text" value={email} placeholder='xyz@pqr.com'></input>
+        <br/>
+        <button>Login</button>
+        </form>
+        <p>Welcome to React Course on codingninjas.com</p>
         <img src={logo} className="App-logo" alt="logo" />
+      
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
